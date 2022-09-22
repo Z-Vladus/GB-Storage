d2553222b53f2ea7327205e91f07fc2a81bf89da
vlad <z.vladuss@gmail.com>
2022-09-23 00:18:26 +0300

    lesson2 TODO 1:45 записи вебинара

--- src/Lesson2/NIO_webinar_code/ChannelReader.java
+++ src/Lesson2/NIO_webinar_code/ChannelReader.java
@@ -1,16 +1,57 @@
 package Lesson2.NIO_webinar_code;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 
 public class ChannelReader {
 
+
+
+    private final SocketChannel channel;
+    private final ByteBuffer buffer;
+    // т.к. данные могут приходить рандомно, будем действовать также,
+    // т.е. сначала передавая размер данных(заголовок со служ.инфой).
+    // определим некий флаг, который будет разделять данные и заголовок
+    private boolean headerReadFlag = true;
+    private byte[] data;
+
     public ChannelReader(SocketChannel channel) {
+        buffer = ByteBuffer.allocate(1024);
+
         this.channel = channel;
     }
 
-    private final SocketChannel channel;
-
     public void read () {
+        // если мы сюда попали, значит, селектор определил что канал доступен для чтения
+        // и в буфере гарантированно , что есть данные
+        try {
+            // читаем
+            channel.read(buffer);
+            // и определяем, первое ли это чтение (чтение заголовка?)
+            if (headerReadFlag) {
+                // если первое, то проверяем, есть ли в буфере 4 байта,
+                // которые дадут нам int - размер данных.
+                System.out.println("ChannelReader.read: expecting header data");
+                if (buffer.position() < 4 ) {
+                    // если данных заголовка мало, уходим, и ждём когда будут 4 байта данных заголовка.
+                    System.out.println("Header not full, waiting for data...");
+                    return;
+                }
+                // если заголовок уже получен, то читаем. Но до этого буфер переводим в режим чтения.
+                buffer.flip();
+                // парсим заголовок в переменную.
+                int size = buffer.getInt();
+                // определяем размер
+                data = new byte[size];
+                // отметим, что флаг уже прочитан.
+                headerReadFlag = false;
+                //TODO 1:45 записи вебинара
+            }
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/Lesson2/NIO_webinar_code/Server.java
+++ src/Lesson2/NIO_webinar_code/Server.java
@@ -41,28 +41,43 @@ public class Server implements Runnable {
     @Override
     public void run() {
         while (true) {
-            // бокирующий метод. Ждём, пока не зарегистрированохотя бы одно событие.
-            // в нашем случае - хотя бы 1 клиент (OP_ACCEPT)
             try {
+                // бокирующий метод select(). Ждём, пока не произойдёт хотя бы одно событие
+                // в нашем случае - хотя бы 1 клиент (OP_ACCEPT)
 
                 selector.select();
+                // это набор, содержащий данные по каналу, и др. служ. инф.
                 Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                // итератор нужен для удаления обработанных ключей, т.е. selector продолжает добавлять
                 Iterator<SelectionKey> iterator = selectionKeys.iterator();
-
+                // обходим коллекцию, достаём каналы, в которых произошли события.
                 while (iterator.hasNext()) {
+                    //извлечение из итератора
                     SelectionKey key = iterator.next();
                     iterator.remove();
+                    // нужна проверка на некую валидность...
                     if (key.isValid()) {
+                        System.out.println("key is not valid, exiting");
                         break;
                     }
+                    System.out.println("key is valid, proceeding");
+                    // проверяем категорию, к которой относится key (какой channel содержит)
+                    // и если это подключение клиента, уходим в метод инициализации подключения
                     if (key.isValid() && key.isAcceptable()) {
+                        System.out.println("key is acceptable, proceeding");
                         acceptClient();
                     }
-
+                    // .. а если это событие чтения, то читаем.
+                    // проблема в том, что обработчик общий, а каналы - разные.
+                    // поэтому надо создавать экземпляры класса, для работы только со своим каналом.
+                    // назовём его ChannelReader
                     if (key.isValid() && key.isReadable()) {
+                        // работаем со своим экземпляром класса ChannelReader
+                        // если его ещё нет, то создаём его
                         if (key.attachment() == null) {
                             key.attach(new ChannelReader((SocketChannel) key.channel()));
                         }
+                        // после прикрепления, читаем из ключа, скастовав ChannelReader
                         ((ChannelReader) key.attachment()).read();
                     }
                 }
@@ -73,10 +88,18 @@ public class Server implements Runnable {
     }
     private void acceptClient(){
         try {
+            // принимаем клиента, получаем ассоциированный с ним socketChannel
             SocketChannel socketChannel = serverSocketChannel.accept();
+            // Далее нужно его также в selector зарегистрировать, сконфигурировать в неблок. режим
             socketChannel.configureBlocking(false);
+            // Регистрируем аналогично, но для мониторинга других событий.
+            // Будем писать или читать.
+            // Конфигурация через битовую маску. OP_READ = 001 ,  OP_WRITE = 100
+            // складываем их побитно, получаем 001 + 100 = 101
             socketChannel.register(selector,SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-            System.out.println("accepted new client");
+            System.out.println("accepted new client ");
+
+
         } catch (IOException e) {
             //throw new RuntimeException(e);
             System.out.println("Client accept failed");
